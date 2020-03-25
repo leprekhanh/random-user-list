8435ee1cd899de684a443adf057f6d144747db15
Khanh <khanh.hoang@live.com>
2020-03-24 22:32:30 -0700

    refactor: Use one function to create and set properties on a node

--- src/js/userInfo.js
+++ src/js/userInfo.js
@@ -8,30 +8,36 @@ export function formatUserData(userArray) {
 	});
 }
 
-function createImageNode(user) {
-	const imageNode = document.createElement("img");
-	imageNode.src = user.imageUrl;
-	imageNode.alt = `Picture of ${user.fullName}`;
-	imageNode.className = "user-image";
-
-	return imageNode;
-}
-
 export function createUserInfoNode(user) {
 	const listItemNode = document.createElement("li");
 	listItemNode.className = "user";
 
-	const userImageNode = createImageNode(user);
-	listItemNode.appendChild(userImageNode);
+	const listItemChildNodes = [
+		{
+			nodeType: "img",
+			className: "user-image",
+			src: user.imageUrl,
+			alt: `Picture of ${user.fullName}`
+		},
+		{
+			nodeType: "div",
+			className: "user-name",
+			innerText: user.fullName
+		},
+		{
+			nodeType: "div",
+			innerText: user.email
+		}
+	];
 
-	const nameNode = document.createElement("div");
-	nameNode.className = "user-name";
-	nameNode.innerText = user.fullName;
-	listItemNode.append(nameNode);
-
-	const emailNode = document.createElement("div");
-	emailNode.innerText = user.email;
-	listItemNode.append(emailNode);
+	listItemChildNodes.forEach(function(childNode) {
+		const { nodeType, ...nodeProperties } = childNode;
+		const node = document.createElement(nodeType);
+		Object.keys(nodeProperties).forEach(
+			nodeProperty => (node[nodeProperty] = childNode[nodeProperty])
+		);
+		listItemNode.append(node);
+	});
 
 	return listItemNode;
 }
